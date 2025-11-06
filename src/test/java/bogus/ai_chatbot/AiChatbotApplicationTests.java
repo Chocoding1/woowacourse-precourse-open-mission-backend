8daee90b7cce0f248c5e739449daa1ecdab73e83
Chocoding1 <galmeagi2@naver.com>
2025-11-06 13:38:57 +0900

    Initial Project

---
+++ src/test/java/bogus/ai_chatbot/AiChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package bogus.ai_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
