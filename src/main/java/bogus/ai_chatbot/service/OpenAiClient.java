4ff681744d18207e3cdc485209c6d1cda17dadf0
Chocoding1 <galmeagi2@naver.com>
2025-11-07 14:15:22 +0900

    Feat(OpenAiClient): OpenAI API 요청 서비스 생성

---
+++ src/main/java/bogus/ai_chatbot/service/OpenAiClient.java
@@ -0,0 +1,53 @@
+package bogus.ai_chatbot.service;
+
+import bogus.ai_chatbot.dto.OpenAiMessage;
+import bogus.ai_chatbot.dto.OpenAiRequest;
+import bogus.ai_chatbot.dto.OpenAiResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OpenAiClient {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${openai.api-url}")
+    private String apiUrl;
+
+    @Value("${openai.model}")
+    private String model;
+
+    public OpenAiResponse getAiResponse(String prompt) {
+        OpenAiRequest openAiRequest = createOpenAiRequest(prompt);
+        log.info("openAirequest : " + openAiRequest);
+
+        ResponseEntity<OpenAiResponse> AiResponse = restTemplate.postForEntity(
+                apiUrl,
+                openAiRequest,
+                OpenAiResponse.class);
+
+        if (!AiResponse.getStatusCode().is2xxSuccessful() || AiResponse.getBody() == null) {
+            throw new RuntimeException("OpenAI API 호출 실패");
+        }
+
+        log.info("OpenAiResponse : " + AiResponse.getBody());
+
+        return AiResponse.getBody();
+    }
+
+    private OpenAiRequest createOpenAiRequest(String prompt) {
+        log.info("model : " + model);
+        OpenAiMessage userMessage = new OpenAiMessage("user", prompt);
+
+        List<OpenAiMessage> messages = List.of(userMessage);
+
+        return new OpenAiRequest(model, messages);
+    }
+}
