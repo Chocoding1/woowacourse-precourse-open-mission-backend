23533039bedd0946f5794d4a5faec7a8befcf36a
Chocoding1 <galmeagi2@naver.com>
2025-11-10 23:21:57 +0900

    Test(EmailServiceTest): 테스트 코드 수정

--- src/test/java/bogus/ai_chatbot/domain/email/service/EmailServiceTest.java
+++ src/test/java/bogus/ai_chatbot/domain/email/service/EmailServiceTest.java
@@ -12,6 +12,7 @@ import static org.mockito.Mockito.when;
 
 import bogus.ai_chatbot.domain.email.dto.EmailDto;
 import bogus.ai_chatbot.domain.member.repository.MemberRepository;
+import bogus.ai_chatbot.domain.redis.service.RedisService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.DisplayName;
@@ -32,10 +33,12 @@ class EmailServiceTest {
     JavaMailSender javaMailSender;
     @Mock
     MemberRepository memberRepository;
+    @Mock
+    RedisService redisService;
 
     @Test
     @DisplayName("인증 코드 전송 성공 테스트")
-    void testMethod() throws MessagingException {
+    void sendAuthCode_success() throws MessagingException {
         //given
         EmailDto emailDto = EmailDto.builder()
                 .email("email")
@@ -45,9 +48,10 @@ class EmailServiceTest {
         when(memberRepository.existsByEmail(anyString())).thenReturn(false);
         when(javaMailSender.createMimeMessage()).thenReturn(mockMessage);
         doNothing().when(javaMailSender).send(any(MimeMessage.class));
+        doNothing().when(redisService).saveEmailAuthCode(anyString(), anyString());
 
         //when
-        emailService.sendVerificationCode(emailDto);
+        emailService.sendAuthCode(emailDto);
 
         //then
         verify(memberRepository, times(1)).existsByEmail(anyString());
@@ -57,7 +61,7 @@ class EmailServiceTest {
 
     @Test
     @DisplayName("이미 가입된 이메일일 경우 예외 발생")
-    void sendVerificationCode_fail_when_duplicate_email() {
+    void sendAuthCode_fail_when_duplicate_email() {
         //given
         EmailDto emailDto = EmailDto.builder()
                 .email("duplicateEmail")
@@ -66,7 +70,7 @@ class EmailServiceTest {
         when(memberRepository.existsByEmail(anyString())).thenReturn(true);
 
         //when & then
-        assertThatThrownBy(() -> emailService.sendVerificationCode(emailDto))
+        assertThatThrownBy(() -> emailService.sendAuthCode(emailDto))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("이미 해당 이메일로 가입한 이력이 있습니다.");
 
