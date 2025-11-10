585571fc3d57c18e07c0764b5ec5a786e081b2bd
Chocoding1 <galmeagi2@naver.com>
2025-11-11 01:38:41 +0900

    Feat(MemberService): 회원가입 시 이메일 인증 확인 로직 추가

--- src/main/java/bogus/ai_chatbot/domain/member/service/MemberService.java
+++ src/main/java/bogus/ai_chatbot/domain/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package bogus.ai_chatbot.domain.member.service;
 
+import bogus.ai_chatbot.domain.email.service.EmailService;
 import bogus.ai_chatbot.domain.member.dto.MemberJoinDto;
 import bogus.ai_chatbot.domain.member.repository.MemberRepository;
 import jakarta.transaction.Transactional;
@@ -14,11 +15,20 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final EmailService emailService;
 
     public void join(MemberJoinDto memberJoinDto) {
+        checkEmailVerified(memberJoinDto.getEmail());
+
         encodeRawPassword(memberJoinDto);
 
         memberRepository.save(memberJoinDto.toEntity());
+    }
+
+    private void checkEmailVerified(String email) {
+        if (!emailService.isEmailVerified(email)) {
+            throw new RuntimeException("이메일 인증이 되지 않았습니다.");
+        }
     }
 
     private void encodeRawPassword(MemberJoinDto memberJoinDto) {
