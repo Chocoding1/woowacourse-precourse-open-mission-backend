d8b2e71f624300b0ac1fbb5d82240fb299ba4b4f
Chocoding1 <galmeagi2@naver.com>
2025-11-10 11:58:55 +0900

    Feat(MemberService): 회원가입 로직 작성

---
+++ src/main/java/bogus/ai_chatbot/service/MemberService.java
@@ -0,0 +1,41 @@
+package bogus.ai_chatbot.service;
+
+import bogus.ai_chatbot.domain.MemberJoinDto;
+import bogus.ai_chatbot.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void join(MemberJoinDto memberJoinDto) {
+        validateDuplicateEmail(memberJoinDto.getEmail());
+
+        encodeRawPassword(memberJoinDto);
+
+        memberRepository.save(memberJoinDto.toEntity());
+    }
+
+    private void validateDuplicateEmail(String email) {
+        if (existSameEmail(email)) {
+            throw new RuntimeException("이미 해당 이메일로 가입한 이력이 있습니다.");
+        }
+    }
+
+    private boolean existSameEmail(String email) {
+        return memberRepository.existsByEmail(email);
+    }
+
+    private void encodeRawPassword(MemberJoinDto memberJoinDto) {
+        String encodedPassword = bCryptPasswordEncoder.encode(memberJoinDto.getPassword());
+        memberJoinDto.setEncodedPassword(encodedPassword);
+    }
+}
