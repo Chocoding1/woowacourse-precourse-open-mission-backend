0561922af113152a1c81be9a6815541e79faa800
Chocoding1 <galmeagi2@naver.com>
2025-11-10 09:50:44 +0900

    Feat(Member): 회원 엔티티 생성

---
+++ src/main/java/bogus/ai_chatbot/domain/Member.java
@@ -0,0 +1,27 @@
+package bogus.ai_chatbot.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String name;
+
+}
+
