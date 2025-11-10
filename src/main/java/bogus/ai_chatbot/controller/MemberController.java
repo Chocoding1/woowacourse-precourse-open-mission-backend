50b41ff86217589c6376b72696b846b22ca3d74b
Chocoding1 <galmeagi2@naver.com>
2025-11-10 12:02:57 +0900

    Feat(MemberContoller): 회원가입 엔드포인트 생성

---
+++ src/main/java/bogus/ai_chatbot/controller/MemberController.java
@@ -0,0 +1,24 @@
+package bogus.ai_chatbot.controller;
+
+import bogus.ai_chatbot.domain.MemberJoinDto;
+import bogus.ai_chatbot.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<?> joinMember(@Validated @RequestBody MemberJoinDto memberJoinDto) {
+        memberService.join(memberJoinDto);
+    }
+}
