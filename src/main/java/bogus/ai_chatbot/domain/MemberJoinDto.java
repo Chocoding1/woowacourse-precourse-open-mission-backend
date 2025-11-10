d3f1dbe9e81740832dfbec5ef1090964378da82c
Chocoding1 <galmeagi2@naver.com>
2025-11-10 11:58:33 +0900

    Feat(MemberJoinDto): 회원가입 요청 DTO 생성

---
+++ src/main/java/bogus/ai_chatbot/domain/MemberJoinDto.java
@@ -0,0 +1,29 @@
+package bogus.ai_chatbot.domain;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class MemberJoinDto {
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+
+    @NotBlank(message = "이름은 필수입니다.")
+    private String name;
+
+    public void setEncodedPassword(String encodedPassword) {
+        password = encodedPassword;
+    }
+
+    public Member toEntity() {
+        return Member.builder()
+                .email(email)
+                .password(password)
+                .name(name)
+                .build();
+    }
+}
