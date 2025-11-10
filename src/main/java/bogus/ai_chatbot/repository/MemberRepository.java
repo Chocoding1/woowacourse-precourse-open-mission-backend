a3238bc1b60329248b68d93284326eb1b4bb49e7
Chocoding1 <galmeagi2@naver.com>
2025-11-10 09:51:05 +0900

    Feat(MemberRepository): 회원 리포지토리 생성

---
+++ src/main/java/bogus/ai_chatbot/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package bogus.ai_chatbot.repository;
+
+import bogus.ai_chatbot.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
