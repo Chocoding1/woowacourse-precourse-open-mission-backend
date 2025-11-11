c62623d882eda1a38e617b411c04aabee7210d76
Chocoding1 <galmeagi2@naver.com>
2025-11-11 16:36:21 +0900

    Refactor(LoginFilter): 객체 변환 로직 메서드로 분리

--- src/main/java/bogus/ai_chatbot/domain/auth/filter/LoginFilter.java
+++ src/main/java/bogus/ai_chatbot/domain/auth/filter/LoginFilter.java
@@ -24,13 +24,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         log.info("LoginFilter -> attemptAuthentication");
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        MemberLoginDto memberLoginDto;
-        try {
-            memberLoginDto = objectMapper.readValue(request.getInputStream(), MemberLoginDto.class);
-        } catch (IOException e) {
-            throw new RuntimeException("이메일 또는 비밀번호가 유효하지 않습니다.");
-        }
+        MemberLoginDto memberLoginDto = getMemberLoginDto(request);
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 memberLoginDto.getEmail(), memberLoginDto.getPassword());
@@ -43,5 +37,14 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
                                             Authentication authentication) {
         log.info("LoginFilter -> successfulAuthentication");
 
+    }
+
+    private static MemberLoginDto getMemberLoginDto(HttpServletRequest request) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(request.getInputStream(), MemberLoginDto.class);
+        } catch (IOException e) {
+            throw new RuntimeException("이메일 또는 비밀번호가 유효하지 않습니다.");
+        }
     }
 }
