af2473fffd73fa8e72b37b3c7d7fb7cebc8051df
Chocoding1 <galmeagi2@naver.com>
2025-11-11 14:49:42 +0900

    Feat(MemberLoginDto): 로그인 DTO 생성

---
+++ src/main/java/bogus/ai_chatbot/domain/member/dto/MemberLoginDto.java
@@ -0,0 +1,17 @@
+package bogus.ai_chatbot.domain.member.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberLoginDto {
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+}
