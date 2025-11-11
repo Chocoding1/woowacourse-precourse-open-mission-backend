d3c63b6f6c79921b0e5daccdd65086dee471ccf3
Chocoding1 <galmeagi2@naver.com>
2025-11-11 14:50:02 +0900

    Feat(LoginFilter): 로그인 필터 생성

---
+++ src/main/java/bogus/ai_chatbot/domain/jwt/LoginFilter.java
@@ -0,0 +1,48 @@
+package bogus.ai_chatbot.domain.jwt;
+
+import bogus.ai_chatbot.domain.member.dto.MemberLoginDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Slf4j
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        log.info("LoginFilter");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        MemberLoginDto memberLoginDto;
+        try {
+            memberLoginDto = objectMapper.readValue(request.getInputStream(), MemberLoginDto.class);
+        } catch (IOException e) {
+            throw new RuntimeException("이메일 또는 비밀번호가 유효하지 않습니다.");
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                memberLoginDto.getEmail(), memberLoginDto.getPassword());
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authentication) {
+        log.info("LoginFilter -> successfulAuthentication");
+
+
+    }
+}
