d5f8f5b448dc3f4e76da05db600901071ddcc67c
Chocoding1 <galmeagi2@naver.com>
2025-11-11 15:35:03 +0900

    Feat(MemberRepository): 이메일로 회원 조회 메서드 생성

--- src/main/java/bogus/ai_chatbot/domain/member/repository/MemberRepository.java
+++ src/main/java/bogus/ai_chatbot/domain/member/repository/MemberRepository.java
@@ -1,9 +1,12 @@
 package bogus.ai_chatbot.domain.member.repository;
 
 import bogus.ai_chatbot.domain.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
