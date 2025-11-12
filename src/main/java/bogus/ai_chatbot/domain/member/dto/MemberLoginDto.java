06c15ef1c30b05685ba1fb331cb42931d9514eed
Chocoding1 <galmeagi2@naver.com>
2025-11-12 22:32:25 +0900

    Refactor(MemberLoginDto): Builder 패턴 추가

--- src/main/java/bogus/ai_chatbot/domain/member/dto/MemberLoginDto.java
+++ src/main/java/bogus/ai_chatbot/domain/member/dto/MemberLoginDto.java
@@ -2,11 +2,15 @@ package bogus.ai_chatbot.domain.member.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class MemberLoginDto {
 
     @NotBlank(message = "이메일은 필수입니다.")
