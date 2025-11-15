05c6ac5a765aad3b1dafae78cb1305337aa172ed
Chocoding1 <galmeagi2@naver.com>
2025-11-16 01:23:30 +0900

    Feat: @EnableJpaAuditing 추가하여 필드 자동 주입 기능 활성화

--- src/main/java/bogus/ai_chatbot/AiChatbotApplication.java
+++ src/main/java/bogus/ai_chatbot/AiChatbotApplication.java
@@ -2,8 +2,10 @@ package bogus.ai_chatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AiChatbotApplication {
 
 	public static void main(String[] args) {
